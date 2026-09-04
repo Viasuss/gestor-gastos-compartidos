7d83d7925a693f5f74b3dc110d88a3450bb7b9d9
wilmer dubuath mantilla rincon <wilmerdubuathmantillarincon@gmail.com>
2026-09-04 16:02:52 -0500

    interfaz de usuario

--- src/main/java/com/gestorgastos/gestor_gastos/controller/UsuarioController.java
+++ src/main/java/com/gestorgastos/gestor_gastos/controller/UsuarioController.java
@@ -9,14 +9,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UsuarioController {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UsuarioController(UsuarioRepository usuarioRepository) {
+    public UsuarioController(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/registro")
@@ -26,27 +30,57 @@ public class UsuarioController {
 
         return "registro";
     }
+
     @PostMapping("/registro")
     public String registrarUsuario(@ModelAttribute Usuario usuario) {
+
+        String passwordCifrada =
+                passwordEncoder.encode(usuario.getPassword());
+
+        usuario.setPassword(passwordCifrada);
 
         usuarioRepository.save(usuario);
 
         return "redirect:/login";
     }
+
     @GetMapping("/login")
     public String mostrarLogin() {
 
         return "login";
     }
+
     @PostMapping("/login")
     public String iniciarSesion(
             @RequestParam String email,
-            @RequestParam String password) {
+            @RequestParam String password,
+            HttpSession session) {
 
         return usuarioRepository.findByEmail(email)
-                .filter(usuario -> usuario.getPassword().equals(password))
-                .map(usuario -> "redirect:/inicio")
+                .filter(usuario -> passwordEncoder.matches(
+                        password,
+                        usuario.getPassword()
+                ))
+                .map(usuario -> {
+                    session.setAttribute("usuario", usuario);
+                    return "redirect:/inicio";
+                })
                 .orElse("redirect:/login?error");
     }
+    @GetMapping("/logout")
+    public String cerrarSesion(HttpSession session) {
 
+        session.invalidate();
+
+        return "redirect:/login";
+    }
+    @GetMapping("/inicio")
+    public String mostrarInicio(HttpSession session) {
+
+        if (session.getAttribute("usuario") == null) {
+            return "redirect:/login";
+        }
+
+        return "inicio";
+    }
 }
